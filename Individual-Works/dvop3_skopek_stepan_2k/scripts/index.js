b9c6bda970d85b281f4d782c90e4ba78358d90d4
StepSkop <74026500+StepSkop@users.noreply.github.com>
2022-04-20 22:49:57 +0200

    Commented

--- Individual-Works/dvop3_skopek_stepan_2k/scripts/index.js
+++ Individual-Works/dvop3_skopek_stepan_2k/scripts/index.js
@@ -1,3 +1,4 @@
+/* Background changing */
 const sections = document.querySelectorAll("section");
 const options = {
     threshold: "0.6",
@@ -24,7 +25,6 @@ const observer = new IntersectionObserver((entries) => {
 sections.forEach((section) => {
     observer.observe(section);
 });
-
 /* Text wrtitng animation */
 var j=0,text;
 text = 'GAME VIBE';
@@ -39,8 +39,6 @@ function typingHome() {
         setTimeout(typingHome,70);
     }
 }
-
-
 var k=0,text2;
 text2 = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil officia praesentium excepturi mollitia sed, repellendus ullam laboriosam dolore accusamus porro repellat cum sint cupiditate aliquid eligendi, sequi quibusdam harum aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ducimus ex nesciunt quam eos quae dolorem, dolore reiciendis amet perspiciatis praesentium dolorum illo ad ipsam beatae? Eos voluptas repudiandae eveniet.';
 function typingAbout() {
@@ -54,8 +52,6 @@ function typingAbout() {
         setTimeout(typingAbout,5);
     }
 }
-
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+/* Mobile VH Fix */
 let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
